e4e64a05004286fca695c9b19e5007629b3cce2c
Kent <kentnsw@gmail.com>
2021-05-23 20:24:47 +1000

    fix test case decoration

--- tests/test_restricted_access.py
+++ tests/test_restricted_access.py
@@ -18,6 +18,7 @@ def test_compliantSg(os_environ, ec2_client):
     assert result == []
 
 
+@mock_ec2
 def test_oneNoncompliantSg(ec2_client, ec2_resource, faker):
     gid = createSg(ec2_client, faker)
     sg = ec2_resource.SecurityGroup(gid)
@@ -26,7 +27,8 @@ def test_oneNoncompliantSg(ec2_client, ec2_resource, faker):
     # case 1: exactly the port
     authSgIngress(gid, 'tcp', 22, 22, [{'CidrIp': '0.0.0.0/0'}], ec2_client)
     result = restricted_access.evaluateSecurityGroups()
-    assert result[0].startswith(f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
+    assert result[0].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
     sg.reload()
     assert len(sg.ip_permissions) == 0
     # after revoked
@@ -36,10 +38,13 @@ def test_oneNoncompliantSg(ec2_client, ec2_resource, faker):
     # case 2: port in range
     authSgIngress(gid, 'tcp', 20, 25, [{'CidrIp': '0.0.0.0/0'}], ec2_client)
     result = restricted_access.evaluateSecurityGroups()
-    assert result[0].startswith(f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
+    assert result[0].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
     sg.reload()
     assert len(sg.ip_permissions) == 0
 
+
+@mock_ec2
 def test_oneNoncompliantSgWithMixedRanges(ec2_client, ec2_resource, faker):
     gid = createSg(ec2_client, faker)
     sg = ec2_resource.SecurityGroup(gid)
@@ -49,30 +54,37 @@ def test_oneNoncompliantSgWithMixedRanges(ec2_client, ec2_resource, faker):
     authSgIngress(gid, 'tcp', 22, 22, [{'CidrIp': '100.0.0.0/0'}], ec2_client)
     result = restricted_access.evaluateSecurityGroups()
     assert len(result) == 1
-    assert result[0].startswith(f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
+    assert result[0].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
     sg.reload()
     assert sg.ip_permissions[0]['IpRanges'][0]['CidrIp'] == '100.0.0.0/0'
 
     # case 4: multi ports on top of case 3
     authSgIngress(gid, 'tcp', 20, 25, [{'CidrIp': '0.0.0.0/0'}], ec2_client)
-    authSgIngress(gid, 'tcp', 3306, 3306, [{'CidrIp': '0.0.0.0/0'}], ec2_client)
+    authSgIngress(gid, 'tcp', 3306, 3306, [
+                  {'CidrIp': '0.0.0.0/0'}], ec2_client)
     result = restricted_access.evaluateSecurityGroups()
     assert len(result) == 2
-    assert result[0].startswith(f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
+    assert result[0].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
     assert result[0].index("\'FromPort\': 20") > 0
-    assert result[1].startswith(f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
+    assert result[1].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg.group_name}({gid})')
     assert result[1].index("\'FromPort\': 3306") > 0
     sg.reload()
     assert sg.ip_permissions[0]['IpRanges'][0]['CidrIp'] == '100.0.0.0/0'
 
 
+@mock_ec2
 def test_multipleNoncompliantSgs(ec2_client, ec2_resource, faker):
     gid1 = createSg(ec2_client, faker)
     gid2 = createSg(ec2_client, faker)
     gid3 = createSg(ec2_client, faker)
     authSgIngress(gid1, 'tcp', 22, 22, [{'CidrIp': '0.0.0.0/0'}], ec2_client)
-    authSgIngress(gid2, 'tcp', 3306, 3306, [{'CidrIp': '0.0.0.0/0'}], ec2_client)
-    authSgIngress(gid3, 'tcp', 3307, 3307, [{'CidrIp': '200.0.0.0/0'}], ec2_client)
+    authSgIngress(gid2, 'tcp', 3306, 3306, [
+                  {'CidrIp': '0.0.0.0/0'}], ec2_client)
+    authSgIngress(gid3, 'tcp', 3307, 3307, [
+                  {'CidrIp': '200.0.0.0/0'}], ec2_client)
     sg1 = ec2_resource.SecurityGroup(gid1)
     sg1.load()
     sg2 = ec2_resource.SecurityGroup(gid2)
@@ -80,8 +92,10 @@ def test_multipleNoncompliantSgs(ec2_client, ec2_resource, faker):
 
     result = restricted_access.evaluateSecurityGroups()
     assert len(result) == 2
-    assert result[0].startswith(f'INFO: Revoked unrestricted access from Security Group {sg1.group_name}({gid1})')
-    assert result[1].startswith(f'INFO: Revoked unrestricted access from Security Group {sg2.group_name}({gid2})')
+    assert result[0].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg1.group_name}({gid1})')
+    assert result[1].startswith(
+        f'INFO: Revoked unrestricted access from Security Group {sg2.group_name}({gid2})')
     sg1.reload()
     sg2.reload()
     assert len(sg1.ip_permissions) == 0
